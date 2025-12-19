3fa225d39c1c455e623cab201bd5bca451c7e9ad
Thirumagal0084 <thirumagal.s2004@gmail.com>
2025-12-19 20:45:01 +0530

    successfully

---
+++ FirstProgram.java
@@ -0,0 +1,12 @@
+package tnsif.introduction;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Thirumagal");
+		System.out.println("Welcome to java");
+
+	}
+
+}
