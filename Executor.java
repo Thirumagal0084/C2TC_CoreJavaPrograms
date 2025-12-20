2d8252eac1b71c40dccc56dc5df4417b8844b439
Thirumagal S <thirumagal.s2004@gmail.com>
2025-12-20 19:56:13 +0530

    DAY -3 Encapsulation Executor

---
+++ Executor.java
@@ -0,0 +1,21 @@
+package tnsif.oopsconcept;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		
+		Encapsulation ED = new Encapsulation();
+		ED.setHouse("Ravenclaw");
+		ED.setFounder("Rowena Ravenclaw");
+		ED.setColour("Blue & Bronze");
+		
+		System.out.println(ED);
+		
+		Executor e = new Executor();
+		System.out.println(e);
+
+	}
+	
+	
+
+}
