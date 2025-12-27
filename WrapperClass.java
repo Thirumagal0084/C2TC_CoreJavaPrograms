c7e553500364a4f16e0666f0f05f57cbe5745ab8
Thirumagal S <thirumagal.s2004@gmail.com>
2025-12-27 10:04:47 +0530

    Wrapper Class in Java

---
+++ WrapperClass.java
@@ -0,0 +1,25 @@
+package tnsif.wrapperclassdemo;
+
+public class WrapperClass {
+
+	public static void main(String[] args) {
+	//Wapper class Example
+
+		
+		int x = 25;
+		
+		Integer y = Integer.valueOf(x);
+		
+		
+		System.out.println(y);
+		
+		
+		//Conversion
+		
+		int z = y;
+		
+		System.out.println(z);
+
+	}
+
+}
